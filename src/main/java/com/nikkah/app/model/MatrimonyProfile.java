9e0e86d7404facef8d82db067a7ff7d1abda3a1e
Mohamed Thoufeeq <pmtthoufeeq@gmail.com>
2023-12-26 21:30:24 +0530

    Rest Api ready

--- src/main/java/com/nikkah/app/model/MatrimonyProfile.java
+++ src/main/java/com/nikkah/app/model/MatrimonyProfile.java
@@ -22,55 +22,7 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class MatrimonyProfile {
-	
-	
-	
-    /**
-	 * 
-	 */
-	public MatrimonyProfile() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	/**
-	 * @param id
-	 * @param firstName
-	 * @param lastName
-	 * @param gender
-	 * @param dateOfBirth
-	 * @param age
-	 * @param address
-	 * @param mobileNumber
-	 * @param email
-	 * @param religion
-	 * @param education
-	 * @param occupation
-	 * @param income
-	 * @param maritalStatus
-	 * @param aboutMe
-	 */
-	public MatrimonyProfile(Long id, String firstName, String lastName, String gender, String dateOfBirth, String age,
-			String address, String mobileNumber, String email, String religion, String education, String occupation,
-			String income, String maritalStatus, String aboutMe) {
-		super();
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.gender = gender;
-		this.dateOfBirth = dateOfBirth;
-		this.age = age;
-		this.address = address;
-		this.mobileNumber = mobileNumber;
-		this.email = email;
-		this.religion = religion;
-		this.education = education;
-		this.occupation = occupation;
-		this.income = income;
-		this.maritalStatus = maritalStatus;
-		this.aboutMe = aboutMe;
-	}
-	
-	
+
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
