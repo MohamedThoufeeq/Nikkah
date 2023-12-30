7f7a5e4628f1470ed315116fb48c0cd495e14773
Mohamed Thoufeeq <pmtthoufeeq@gmail.com>
2023-12-30 18:40:28 +0530

    updated

--- src/main/java/com/nikkah/app/controller/MatrimonyProfileController.java
+++ src/main/java/com/nikkah/app/controller/MatrimonyProfileController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,6 +30,7 @@ import com.nikkah.app.service.MatrimonyProfileService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("*")
 public class MatrimonyProfileController {
 
 	@Autowired
